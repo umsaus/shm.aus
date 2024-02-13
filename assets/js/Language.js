62ef8d8f065da9c20cf7250af6ec4dd528e0d582
umsaus <159860201+umsaus@users.noreply.github.com>
2024-02-13 03:33:15 +0200

    Add files via upload

---
+++ assets/js/Language.js
@@ -0,0 +1,11 @@
+function SelectedIndexChanged(s) {
+    setCookie("Culture", s);
+
+}
+//to handle multi language
+function setCookie(cname, cvalue) {
+    var d = new Date();
+    d.setTime(d.getTime() + (24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
